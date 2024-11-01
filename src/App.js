1fc530776f9173be176a83d9f0a946f69ca6bc8f
RayIwobi <techlodas@gmail.com>
2024-11-01 17:25:31 +0100

    first-commit
    This is a simple calculator created with react to showcase its basic functionality.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CalculatorNav from './Components/CalculatorNav';
+import Calculator from './Components/Calculator';
+import Lottie from 'lottie-react'
+import animo from './Components/assets/animo.json'
+import Notie from './Components/Notie'
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    
+      <CalculatorNav />
+      <Notie/>
+      <Routes>
+        <Route path='/' element={<Calculator />} />
+      </Routes>
+      <Lottie animationData={animo} style={{width:'300px', height:'300px'}} />
+      
+    </Router>
+    </>
   );
 }
 
